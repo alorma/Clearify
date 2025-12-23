9e34e96e790274ced894a768c193e1e8b0be0f5c
Bernat Borrás <bernatbor15@gmail.com>
2025-12-23 21:49:14 +0100

    Safe

--- composeApp/src/wasmJsMain/kotlin/com/alorma/clearify/ClipboardManager.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/alorma/clearify/ClipboardManager.wasmJs.kt
@@ -2,9 +2,12 @@ package com.alorma.clearify
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class WasmJsClipboardManager : ClipboardManager {
-    override fun copyToClipboard(text: String) {
+    override suspend fun copyToClipboard(text: String) {
         copyToClipboardExternal(text)
     }
 }
@@ -14,6 +17,10 @@ actual fun rememberClipboardManager(): ClipboardManager {
     return remember { WasmJsClipboardManager() }
 }
 
-private fun copyToClipboardExternal(text: String) {
-    js("navigator.clipboard.writeText(text)")
+private suspend fun copyToClipboardExternal(text: String) = suspendCoroutine<Unit> { cont ->
+    try {
+        js("navigator.clipboard.writeText(text).then(() => cont.resume(kotlin.Unit)).catch((e) => cont.resumeWithException(new Error(e)))")
+    } catch (e: Throwable) {
+        cont.resumeWithException(e)
+    }
 }
